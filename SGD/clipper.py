69dbc8e20c00659f41d2f7c699e446099b67a1c8
Floating <fudongfdzhang@gmail.com>
2025-10-15 09:20:33 +0800

    Update clipper.py

--- SGD/clipper.py
+++ SGD/clipper.py
@@ -4,9 +4,9 @@ import numpy as np
 
 class ZeroOneClipper(object):
     def __call__(self, module):
-        # filter the variables to get the ones you want
+     
         if hasattr(module, 'alpha'):
-            # module.alpha.data = torch.clamp(module.alpha.data, 1/(np.e**(1/4)), 0.995)  # Clip tau to [4*time_step, ]
+          
             module.alpha.data.clamp_(2/np.e, 0.995)
         if hasattr(module, 'beta'):
             module.beta.data.clamp_(2/np.e, 0.995)
